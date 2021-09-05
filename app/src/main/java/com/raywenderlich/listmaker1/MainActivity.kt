916dffe70e1e0cd7176d22d3e89c7e0777c4b8fb
Jabbanero <jabbascript@gmail.com>
2021-09-05 17:42:46 -0400

    .

--- app/src/main/java/com/raywenderlich/listmaker1/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/listmaker1/MainActivity.kt
@@ -5,16 +5,27 @@ import android.os.Bundle
 import android.text.InputType
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.ViewModelProvider
+import androidx.preference.PreferenceManager
 import com.raywenderlich.listmaker1.databinding.MainActivityBinding
 import com.raywenderlich.listmaker1.ui.main.MainFragment
+import com.raywenderlich.listmaker1.ui.main.MainViewModel
+import com.raywenderlich.listmaker1.ui.main.MainViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: MainActivityBinding
+    private lateinit var viewModel: MainViewModel
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        viewModel = ViewModelProvider(this,
+            MainViewModelFactory(PreferenceManager.getDefaultSharedPreferences(this)))
+            .get(MainViewModel::class.java)
 
         binding = MainActivityBinding.inflate(layoutInflater)
         val view = binding.root
@@ -28,22 +39,28 @@ class MainActivity : AppCompatActivity() {
 
         binding.fabButton.setOnClickListener{
             showCreateListDialog()
+            }
         }
-    }
 
     private fun showCreateListDialog() {
 
+        //retrieve strings
         val dialogTitle = getString(R.string.name_of_list)
         val positiveButtonTitle = getString(R.string.create_list)
 
+        //create alert dialog
         val builder = AlertDialog.Builder(this)
         val listTitleEditText = EditText(this)
         listTitleEditText.inputType = InputType.TYPE_CLASS_TEXT
         builder.setTitle(dialogTitle)
         builder.setView(listTitleEditText)
 
+        //add positive button
         builder.setPositiveButton(positiveButtonTitle) { dialog, _ -> dialog.dismiss() }
+        //create a new list
+        viewModel.saveList(TaskList(listTitleEditText.text.toString()))
 
+        //display dialog
         builder.create().show()
     }
 }
